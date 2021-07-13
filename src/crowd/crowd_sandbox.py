9e26ffc35540f9f0784b38abd12205592d7ccb35
SirGnip <SirGnip@users.noreply.github.com>
2021-07-13 14:00:31 -0500

    Add timing measurements

--- src/crowd/crowd_sandbox.py
+++ src/crowd/crowd_sandbox.py
@@ -7,6 +7,7 @@ sophisticated collision resolution logic as this is just an experiment.
 import random
 import sys
 import time
+import statistics
 from typing import Optional
 
 import arcade
@@ -16,72 +17,88 @@ from crowd import bots
 from common.fpsscanner import FpsScanner
 from common import utl
 from common.fpscounter import FpsCounter
+from common.timer import Timer
 
 
 class MyGame(arcade.Window):
     def __init__(self):
-        self.cnt = 0
-        super().__init__(800, 600, sys.argv[0])
-        self.paused = False
-        self.frame_advance = False
-        self.sleep: Optional[float] = None
-        self.click_mode = 'goal'
-        self.clicked_bot = None
-        self.scanner = FpsScanner()
-        self.fps = FpsCounter(120)
-        self.bots = arcade.SpriteList()
-        self.bot_factories = utl.Cycler((
-            (arcade.color.RED, bots.Bot),
-            (arcade.color.DARK_GRAY, bots.StationaryBot),
-            (arcade.color.YELLOW, bots.OctWalkBot),
-            (arcade.color.GREEN, bots.RandomWalkBot),
-            (arcade.color.BLUE, bots.BounceBot),
-            (arcade.color.PURPLE, bots.RunAwayBot),
-        ))
+        self.times_init = []
+        self.times_draw = []
+        self.times_update = []
+        self.total_timer = Timer()
+        self.total_timer.start()
 
-        goal = _Vec2(700, 300)
-        seq = 0
-        for x in range(50, 150, 25):
-            for y in range(50, 550, 25):
-                b = bots.Bot(x, y, self.bots, arcade.color.RED)
-                b.id = seq
-                seq += 1
-                b.set_goal(goal)
+        with Timer() as init_timer:
+            self.cnt = 0
+            super().__init__(800, 600, sys.argv[0])  # update_rate=1/60
+            self.paused = False
+            self.frame_advance = False
+            self.sleep: Optional[float] = None
+            self.click_mode = 'goal'
+            self.clicked_bot = None
+            self.scanner = FpsScanner()
+            self.fps = FpsCounter(120)
+            self.bots = arcade.SpriteList()
+            self.bot_factories = utl.Cycler((
+                (arcade.color.RED, bots.Bot),
+                (arcade.color.DARK_GRAY, bots.StationaryBot),
+                (arcade.color.YELLOW, bots.OctWalkBot),
+                (arcade.color.GREEN, bots.RandomWalkBot),
+                (arcade.color.BLUE, bots.BounceBot),
+                (arcade.color.PURPLE, bots.RunAwayBot),
+            ))
+
+            goal = _Vec2(700, 300)
+            seq = 0
+            for x in range(50, 150, 25):
+                for y in range(50, 550, 25):
+                    b = bots.Bot(x, y, self.bots, arcade.color.RED)
+                    b.id = seq
+                    seq += 1
+                    b.set_goal(goal)
+                    self.bots.append(b)
+            self.bots[9].angle = 355
+
+            self.bots.append(bots.OctWalkBot(400, 300, self.bots, arcade.color.YELLOW))
+            self.bots.append(bots.RunAwayBot(500, 350, self.bots, arcade.color.PURPLE))
+            self.bots.append(bots.RunAwayBot(475, 340, self.bots, arcade.color.PURPLE))
+
+            for x in range(550, 650, 25):
+                for y in range(450, 550, 25):
+                    self.bots.append(bots.RandomWalkBot(x, y, self.bots, arcade.color.GREEN))
+
+            for x in (500, 600, 625, 650, 700):
+                b = bots.BounceBot(x, 300, self.bots, arcade.color.BLUE)
+                b.angle = 180
                 self.bots.append(b)
-        self.bots[9].angle = 355
 
-        self.bots.append(bots.OctWalkBot(400, 300, self.bots, arcade.color.YELLOW))
-        self.bots.append(bots.RunAwayBot(500, 350, self.bots, arcade.color.PURPLE))
-        self.bots.append(bots.RunAwayBot(475, 340, self.bots, arcade.color.PURPLE))
+            for y in range(200, 400, 20):
+                self.bots.append(bots.StationaryBot(723, y, self.bots, arcade.color.DARK_GRAY))
 
-        for x in range(550, 650, 25):
-            for y in range(450, 550, 25):
-                self.bots.append(bots.RandomWalkBot(x, y, self.bots, arcade.color.GREEN))
+            print(f'There are {len(self.bots)} starting Bots')
 
-        for x in (500, 600, 625, 650, 700):
-            b = bots.BounceBot(x, 300, self.bots, arcade.color.BLUE)
-            b.angle = 180
-            self.bots.append(b)
-
-        for y in range(200, 400, 20):
-            self.bots.append(bots.StationaryBot(723, y, self.bots, arcade.color.DARK_GRAY))
+        self.times_init.append(init_timer.last_elapsed)
 
     def on_draw(self):
-        arcade.start_render()
-        self.scanner.draw()
-        self.bots.draw()
+        with Timer(logger=None) as draw_timer:
+            arcade.start_render()
+            self.scanner.draw()
+            self.bots.draw()
+        self.times_draw.append(draw_timer.last_elapsed)
 
     def update(self, delta_time: float):
-        self.fps.tick()
-        if self.fps.is_ready():
-            print('FPS', self.fps.get_fps())
-        if not self.paused or self.frame_advance:
-            if self.frame_advance:
-                self.frame_advance = False
-            if self.sleep is not None:
-                time.sleep(self.sleep)
-            self.scanner.update()
-            self.bots.update()
+        with Timer(logger=None) as update_timer:
+            self.fps.tick()
+            if self.fps.is_ready():
+                print('FPS', self.fps.get_fps())
+            if not self.paused or self.frame_advance:
+                if self.frame_advance:
+                    self.frame_advance = False
+                if self.sleep is not None:
+                    time.sleep(self.sleep)
+                self.scanner.update()
+                self.bots.update()
+        self.times_update.append(update_timer.last_elapsed)
 
     def on_key_press(self, symbol: int, modifiers: int):
         # pause
@@ -116,8 +133,24 @@ class MyGame(arcade.Window):
             self.click_mode = 'move'
             self.clicked_bot = None
         elif symbol == arcade.key.ESCAPE:
+            self.on_closing()
             self.close()
         print('Mouse Click Mode:', self.click_mode)
+
+    def _times_summary(self, tag, times):
+        min_times = min(times)
+        max_times = max(times)
+        sum_times = sum(times)
+        mode_times = statistics.mode(times)
+        avg_times = statistics.mean(times)
+        txt = '{} {:0.6f} {:0.6f} {:0.6f} {:0.6f} {:0.6f}'.format(tag, min_times, mode_times, avg_times, max_times, sum_times)
+        print(txt)
+
+    def on_closing(self):
+        self._times_summary('init  ', self.times_init)
+        self._times_summary('draw  ', self.times_draw)
+        self._times_summary('update', self.times_update)
+        print('total', self.total_timer.stop())
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         super().on_mouse_press(x, y, button, modifiers)
